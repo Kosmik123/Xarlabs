792a6df2b3eab3abe5808e893fe035f723155451
Kosmik <malpa.w.klapkach@gmail.com>
2025-04-30 14:53:20 +0200

    Add cone part creation to DropProceduralMesh

--- Assets/Scripts/Procedural Mesh Creation/DropProceduralMeshAsset.cs
+++ Assets/Scripts/Procedural Mesh Creation/DropProceduralMeshAsset.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ProceduralMeshCreation
 {
@@ -6,7 +7,7 @@ namespace ProceduralMeshCreation
     public class DropProceduralMeshAsset : ProceduralMeshAsset
     {
         [SerializeField]
-        private Vector2Int resolution; 
+        private Vector2Int resolution;
 
         [SerializeField]
         private float radius;
@@ -15,7 +16,36 @@ namespace ProceduralMeshCreation
 
         public override void BuildMesh(Mesh mesh)
         {
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
+            
+            
+            var tipEnd = Vector3.up * (radius + tipLength);
+            vertices.Add(tipEnd);
 
+            float angleDelta = 360f / resolution.x;
+            for (int i = 0; i < resolution.x; i++)
+            {
+                float angle = angleDelta * i;
+                var radialVertex = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward * radius;
+                vertices.Add(radialVertex);
+
+                int next = i == resolution.x - 1 
+                    ? 1 
+                    : i + 2;
+                AddTriangle(0, i + 1, next);
+            }
+
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+
+            void AddTriangle(int a, int b, int c)
+            {
+                triangles.Add(a);
+                triangles.Add(b);
+                triangles.Add(c);
+            }
         }
 
         protected override void OnValidate()
