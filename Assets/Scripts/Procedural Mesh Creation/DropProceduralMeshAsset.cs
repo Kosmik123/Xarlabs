a6d1e138852f9dbf6c71d3e29ad9232d3710a287
Kosmik <malpa.w.klapkach@gmail.com>
2025-04-30 03:07:48 +0200

    Add class for drop procedural mesh creation

---
+++ Assets/Scripts/Procedural Mesh Creation/DropProceduralMeshAsset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ProceduralMeshCreation
+{
+    [CreateAssetMenu(menuName = "Procedural Mesh Creation/Drop Procedural Mesh")]
+    public class DropProceduralMeshAsset : ProceduralMeshAsset
+    {
+        [SerializeField]
+        private Vector2Int resolution; 
+
+        [SerializeField]
+        private float radius;
+        [SerializeField]
+        private float tipLength;
+
+        public override void BuildMesh(Mesh mesh)
+        {
+
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            resolution.x = Mathf.Max(3, resolution.x);
+            resolution.y = Mathf.Max(3, resolution.y);
+
+        }
+
+    }
+}
