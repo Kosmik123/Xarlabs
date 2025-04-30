dbca7c5557502fdd5fdacd861110899d886025b4
Kosmik <malpa.w.klapkach@gmail.com>
2025-05-01 00:49:52 +0200

    Finalize the sphere shape generation

--- Assets/Scripts/Procedural Mesh Creation/DropProceduralMeshAsset.cs
+++ Assets/Scripts/Procedural Mesh Creation/DropProceduralMeshAsset.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.ComponentModel;
+using UnityEngine;
 using UnityEngine.Pool;
 
 namespace ProceduralMeshCreation
@@ -19,34 +20,77 @@ namespace ProceduralMeshCreation
 
         public override void BuildMesh(Mesh mesh)
         {
+            var vertices = ListPool<Vector3>.Get();
+            var triangles = ListPool<int>.Get();
+
             Vector3 tipDirection = direction.normalized;
             Vector3 equatorRadiusAxis = GetPerpendicularDirection(tipDirection);
             Vector3 rotationAxis = Vector3.Cross(tipDirection, equatorRadiusAxis).normalized;
 
-            // Cone
-
             float tipDistanceFromCenter = radius + tipLength;
             float tangentAngle = Mathf.Asin(radius / tipDistanceFromCenter) * Mathf.Rad2Deg;
-
+            float longitudeDelta = 360f / resolution.x;
             Vector3 coneBaseRadiusDirection = Quaternion.AngleAxis(-tangentAngle, rotationAxis) * equatorRadiusAxis;
 
-            var vertices = ListPool<Vector3>.Get();
-            var triangles = ListPool<int>.Get();
-
-            var tipEnd = tipDirection * tipDistanceFromCenter;
-            vertices.Add(tipEnd);
-
-            float angleDelta = 360f / resolution.x;
-            for (int i = 0; i < resolution.x; i++)
+            Cone();
+            void Cone()
             {
-                float angle = angleDelta * i;
-                var radialVertex = Quaternion.AngleAxis(angle, tipDirection) * coneBaseRadiusDirection * radius;
-                vertices.Add(radialVertex);
+                var tipEnd = tipDirection * tipDistanceFromCenter;
+                vertices.Add(tipEnd);
 
-                int next = i == resolution.x - 1
-                    ? 1
-                    : i + 2;
-                AddTriangle(0, i + 1, next);
+                for (int i = 0; i < resolution.x; i++)
+                {
+                    float angle = longitudeDelta * i;
+                    var radialVertex = Quaternion.AngleAxis(angle, tipDirection) * coneBaseRadiusDirection * radius;
+                    vertices.Add(radialVertex);
+
+                    int next = i == resolution.x - 1
+                        ? 1
+                        : i + 2;
+                    AddTriangle(0, i + 1, next);
+                }
+            }
+
+            Sphere();
+            void Sphere()
+            {
+                float latitudeFullAngle = 90 + tangentAngle;
+                float latitudeDelta = latitudeFullAngle / resolution.y;
+                Vector3 pole = -tipDirection * radius;
+                for (int j = 1; j < resolution.y; j++)
+                {
+                    float latitude = j * latitudeDelta;
+                    Vector3 latitudeRadiusDirection = Quaternion.AngleAxis(latitude, rotationAxis) * coneBaseRadiusDirection;
+                    for (int i = 0; i < resolution.x; i++)
+                    {
+                        float angle = longitudeDelta * i;
+                        var radialVertex = Quaternion.AngleAxis(angle, tipDirection) * latitudeRadiusDirection * radius;
+                        vertices.Add(radialVertex);
+
+                        bool isLast = i == resolution.x - 1;
+                        int right = i + 1;
+                        int left = right % resolution.x + 1;
+                        int top = (j - 1) * resolution.x;
+                        int bottom = j * resolution.x;
+
+                        int topLeft = top + left;
+                        int topRight = top + right;
+                        int bottomLeft = bottom + left;
+                        int bottomRight = bottom + right;
+
+                        AddQuad(bottomLeft, bottomRight, topLeft, topRight);
+                    }
+                }
+
+                vertices.Add(pole);
+                int lastIndex = vertices.Count - 1;
+                int baseIndex = lastIndex - resolution.x;
+                for (int i = 0; i < resolution.x; i++)
+                {
+                    int right  = baseIndex + i;
+                    int left = (i + 1) % resolution.x + baseIndex;
+                    AddTriangle(left, right, lastIndex);
+                }
             }
 
             mesh.Clear();
@@ -62,6 +106,12 @@ namespace ProceduralMeshCreation
                 triangles.Add(a);
                 triangles.Add(b);
                 triangles.Add(c);
+            }
+
+            void AddQuad(int bottomLeft, int bottomRight, int topLeft, int topRight)
+            {
+                AddTriangle(bottomLeft, topLeft, bottomRight);
+                AddTriangle(bottomRight, topLeft, topRight);
             }
         }
 
