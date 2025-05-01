41766e84ae93c9ff09c9426b6ede8f77310841a6
Kosmik <malpa.w.klapkach@gmail.com>
2025-05-01 17:20:10 +0200

    Add rotation towards target with limited angular speed

---
+++ Assets/Scripts/LimitedSpeedLookAt.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LimitedSpeedLookAt : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    public Transform Target
+    {
+        get => target;
+        set => target = value;
+    }
+
+    [SerializeField]
+    private float angularSpeed = 200;
+    public float AngularSpeed
+    {
+        get => angularSpeed;
+        set => angularSpeed = value;
+    }
+
+    private void Update()
+    {
+        var transform = this.transform;
+        var direction = target.position - transform.position;
+
+        float angle = angularSpeed * Mathf.Deg2Rad * Time.deltaTime;
+        transform.forward = Vector3.RotateTowards(transform.forward, direction, angle, 0);
+    }
+}
